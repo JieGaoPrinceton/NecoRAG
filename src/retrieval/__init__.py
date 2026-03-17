6562de378e2b1de2a1416000e0b3e9c3f8743a2d
qijie2026 <16608619+qijie2026@user.noreply.gitee.com>
2026-03-17 18:22:29 +0000

    Upload src/retrieval/__init__.py

---
+++ src/retrieval/__init__.py
@@ -0,0 +1,18 @@
+"""
+Pounce Strategy - 扑击检索策略
+检索层核心组件，智能化的信息检索与重排序
+"""
+
+from src.retrieval.retriever import PounceRetriever
+from src.retrieval.hyde import HyDEEnhancer
+from src.retrieval.reranker import ReRanker
+from src.retrieval.fusion import FusionStrategy
+from src.retrieval.models import RetrievalResult
+
+__all__ = [
+    "PounceRetriever",
+    "HyDEEnhancer",
+    "ReRanker",
+    "FusionStrategy",
+    "RetrievalResult",
+]
